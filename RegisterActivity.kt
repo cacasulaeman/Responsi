7054754816074f36a47637a0fa7c04c71e558325
cacasulaeman <55708209+cacasulaeman@users.noreply.github.com>
2019-10-29 10:10:40 +0700

    Add files via upload

---
+++ RegisterActivity.kt
@@ -0,0 +1,12 @@
+package Responsi.Application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
